6c44938a49473ebec4f726c50ba10596fc1a29f8
RazMunir <rm.c151010@gmail.com>
2020-11-04 12:02:19 +0600

    toast showed

--- app/src/main/java/com/example/testapplication/MainActivity.java
+++ app/src/main/java/com/example/testapplication/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Fly {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +16,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void showToast(View view) {
         Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void goForward() {
+        Toast.makeText(this, "Go Forward", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void goDown() {
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
     }
 }
