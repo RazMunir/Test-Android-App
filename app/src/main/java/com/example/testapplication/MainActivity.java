069a7d952a0ccb5c01e2f912020b516f5d6afe20
RazMunir <rm.c151010@gmail.com>
2020-11-04 14:35:16 +0600

    practiced

--- app/src/main/java/com/example/testapplication/MainActivity.java
+++ app/src/main/java/com/example/testapplication/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements Fly{
 
     Duck duck;
     Finch finch;
@@ -37,6 +37,7 @@ public class MainActivity extends AppCompatActivity{
         Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show();
     }
 
+    //for duck
     public void goForward(View view) {
         duck.goForward();
     }
@@ -45,11 +46,31 @@ public class MainActivity extends AppCompatActivity{
         duck.goDown();
     }
 
+    //for finch
     public void finchGoForward(View view) {
         finch.finchGoForward();
     }
 
     public void finchGoDown(View view) {
         finch.finchGoDown();
+    }
+
+    //for main activity
+    @Override
+    public void goForward() {
+        Toast.makeText(this, "Go Forward in Main Activity", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void goDown() {
+        Toast.makeText(this, "Go Down in Main Activity", Toast.LENGTH_SHORT).show();
+    }
+
+    public void MainActGoForward(View view) {
+        goForward();
+    }
+
+    public void MainActGoDown(View view) {
+        goDown();
     }
 }
